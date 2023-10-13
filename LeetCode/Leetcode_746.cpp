50d65f0d5675eb7c457398efac7058767baf1fa7
LoppA <xLopacheco@gmail.com>
2023-10-13 21:21:59 +0100

    Add leetcode 746

---
+++ LeetCode/Leetcode_746.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        for (int i = n-3; i >= 0; i--) {
+            cost[i] += min(cost[i+1], cost[i+2]);
+        }
+        
+        return min(cost[0], cost[1]);
+    }
+};
