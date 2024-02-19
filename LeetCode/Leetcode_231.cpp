5fb65b619c8924997a492c517e510e9339d339f7
LoppA <xLopacheco@gmail.com>
2024-02-19 10:29:53 +0000

    Add Leetcode 231

---
+++ LeetCode/Leetcode_231.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/power-of-two/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+      if (n <= 0) {
+        return false;
+      }
+      return __builtin_popcount(n) == 1;
+    }
+};
